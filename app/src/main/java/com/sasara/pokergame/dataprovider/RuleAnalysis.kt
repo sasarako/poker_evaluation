c927bc1f9b1ba69bacffbc0640375f6d3eb8198d
Sirisub Amnartsirisuk <sasara.1412@gmail.com>
2018-01-25 17:03:45 +0700

    Remove local variable not keep state.

--- app/src/main/java/com/sasara/pokergame/dataprovider/RuleAnalysis.kt
+++ app/src/main/java/com/sasara/pokergame/dataprovider/RuleAnalysis.kt
@@ -13,18 +13,14 @@ interface ResultInterface<T> {
     fun getAnalysisResult(cardList: List<Card>): Observable<T>
 }
 
-class RuleAnalysis() : ResultInterface<OnHandResult> {
-
-    private var fiveSortedCards: List<Card> = mutableListOf()
-    private var groupedAndSortedCards: List<Pair<Int, List<Card>>> = mutableListOf()
-
+class RuleAnalysis : ResultInterface<OnHandResult> {
 
     override fun getAnalysisResult(cardList: List<Card>): Observable<OnHandResult> {
-        fiveSortedCards = cardList.sortedBy { it.getRank() }
+        val fiveSortedCards = cardList.sortedBy { it.getRank() }
 
         //Group by rank value and sorted them convert to List of pair<Rank,Size>
         //Sample 4S 5S 4C 5C 9H = ("9",size = 1),("4",size = 2) , ("5", size = 2)
-        groupedAndSortedCards = fiveSortedCards.groupBy({ it.getRank() }).toSortedMap().toList().
+        val groupedAndSortedCards = fiveSortedCards.groupBy({ it.getRank() }).toSortedMap().toList().
                 sortedWith(compareBy({ it.second.size }, { it.first }))
 
         //Generate secondary compare value
@@ -34,15 +30,15 @@ class RuleAnalysis() : ResultInterface<OnHandResult> {
 
         val type: PokerHandType =
                 when {
-                    isStraightFlush() -> PokerHandType.STRAIGHT_FLUSH
-                    isFourOfAKind() -> PokerHandType.FOUR_OF_A_KIND
-                    isFullHouse() -> PokerHandType.FULL_HOUSE
-                    isFlush() -> PokerHandType.FLUSH
-                    isStraight() -> PokerHandType.STRAIGHT
-                    isThreeOfAKind() -> PokerHandType.THREE_OF_A_KIND
-                    isTwoPairs() -> PokerHandType.TWO_PAIRS
-                    isOnePair() -> PokerHandType.PAIR
-                    isHighCard() -> PokerHandType.HIGH_CARD
+                    isStraightFlush(fiveSortedCards) -> PokerHandType.STRAIGHT_FLUSH
+                    isFourOfAKind(groupedAndSortedCards) -> PokerHandType.FOUR_OF_A_KIND
+                    isFullHouse(groupedAndSortedCards) -> PokerHandType.FULL_HOUSE
+                    isFlush(fiveSortedCards) -> PokerHandType.FLUSH
+                    isStraight(fiveSortedCards) -> PokerHandType.STRAIGHT
+                    isThreeOfAKind(groupedAndSortedCards) -> PokerHandType.THREE_OF_A_KIND
+                    isTwoPairs(groupedAndSortedCards) -> PokerHandType.TWO_PAIRS
+                    isOnePair(groupedAndSortedCards) -> PokerHandType.PAIR
+                    isHighCard(groupedAndSortedCards) -> PokerHandType.HIGH_CARD
                     else -> PokerHandType.UNDEFINED
                 }
 
@@ -50,15 +46,14 @@ class RuleAnalysis() : ResultInterface<OnHandResult> {
         //CompareRanks is secondary factor to compare
         return Observable.just(OnHandResult(type = type,
                 compareRanks = compareRanks))
-
     }
 
 
-    private fun isStraightFlush(): Boolean {
-        return isFlush() && isStraight()
+    private fun isStraightFlush(fiveSortedCards: List<Card>): Boolean {
+        return isFlush(fiveSortedCards) && isStraight(fiveSortedCards)
     }
 
-    private fun isFlush(): Boolean {
+    private fun isFlush(fiveSortedCards: List<Card>): Boolean {
         if (fiveSortedCards.size < 5) {
             return false
         }
@@ -66,7 +61,7 @@ class RuleAnalysis() : ResultInterface<OnHandResult> {
         return fiveSortedCards.all { it.getSuit() == fiveSortedCards[0].getSuit() }
     }
 
-    private fun isStraight(): Boolean {
+    private fun isStraight(fiveSortedCards: List<Card>): Boolean {
 
         val fiveSortedCopied = fiveSortedCards.sortedBy {
             it.getRank()
@@ -89,37 +84,37 @@ class RuleAnalysis() : ResultInterface<OnHandResult> {
         return true
     }
 
-    private fun isFourOfAKind(): Boolean {
+    private fun isFourOfAKind(groupedAndSortedCards: List<Pair<Int, List<Card>>>): Boolean {
         //Amount of grouped with rank
         //Same value = 4
         return groupedAndSortedCards.size == 2 && groupedAndSortedCards[1].second.size == 4
     }
 
-    private fun isFullHouse(): Boolean {
+    private fun isFullHouse(groupedAndSortedCards: List<Pair<Int, List<Card>>>): Boolean {
         //Amount of grouped with rank
         //Same primary value = 3 and size of grouping = 2 (with 1 pair same rank)
         return groupedAndSortedCards.size == 2 && groupedAndSortedCards[1].second.size == 3
     }
 
-    private fun isThreeOfAKind(): Boolean {
+    private fun isThreeOfAKind(groupedAndSortedCards: List<Pair<Int, List<Card>>>): Boolean {
         //Amount of grouped with rank
         //Same primary value = 3 and size of grouping = 3 (without another same rank)
         return groupedAndSortedCards.size == 3 && groupedAndSortedCards[2].second.size == 3
     }
 
-    private fun isTwoPairs(): Boolean {
+    private fun isTwoPairs(groupedAndSortedCards: List<Pair<Int, List<Card>>>): Boolean {
         //Amount of grouped with rank
         //Same primary value = 2 and size of grouping = 2
         return groupedAndSortedCards.size == 3 && groupedAndSortedCards[2].second.size == 2
     }
 
-    private fun isOnePair(): Boolean {
+    private fun isOnePair(groupedAndSortedCards: List<Pair<Int, List<Card>>>): Boolean {
         //Amount of grouped with rank
         //Same primary value = 2 and size of grouping = 4 (without another same rank)
         return groupedAndSortedCards.size == 4
     }
 
-    private fun isHighCard(): Boolean {
+    private fun isHighCard(groupedAndSortedCards: List<Pair<Int, List<Card>>>): Boolean {
         return groupedAndSortedCards.size == 5
     }
 }
